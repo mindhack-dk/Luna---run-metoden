ccd6d9f45a844fb620fc4c4f735c76f4cf5fcae0
Patrick Agergaard <patrickagergaard@gmail.com>
2025-03-15 15:22:24 +0100

    Livedemo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Main {
+  public static void main(String[] args) {
+    // Vi skriver KUN denne linje i main.
+    // Derved laves en INSTANS af klassen, så vi kan kalde ikke-static metoder på instansen.
+    // SÅ slipper vi for at lave alting static, hvilket nemlig er noget rod.
+
+    new Main().run();
+  }
+
+  private void run() {
+    // Vi skriver al vores kode HER i stedet for i main(), som jo er static
+    tilføjMøbel();
+  }
+
+  public void tilføjMøbel() {
+    //
+  }
 }
